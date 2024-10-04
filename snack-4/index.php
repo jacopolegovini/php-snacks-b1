0e0e7e7aaf999f1eed7b969ebf8ca5fc0ae52f07
jacopolegovini <legovini96@gmail.com>
2024-10-04 16:27:39 +0200

    filter max vote added

--- snack-4/index.php
+++ snack-4/index.php
@@ -1,12 +1,11 @@
 <?php
 include "./data.php";
 
+// ! Codice per filtrare esclusivamente gli studenti con voti superiori al 6.
 $bestStudents = [];
 $bestStudentsInClass = [];
 $bestStudentsInClasses = [];
 
-
-// Filtriamo esclusivamente gli studenti con media superiore a 6.
 // foreach ($classi as $classElementsValue) {
 //     foreach ($classElementsValue as $classMember) {
 //         if ($classMember['voto_medio'] >= 6) {
@@ -20,17 +19,24 @@ $bestStudentsInClasses = [];
 
 // $classi = $bestStudentsInClasses;
 
-?>
 
-<!-- Classe 1A" => [
-        [
-            "id" => 1,
-            "nome" => "Marco",
-            "cognome" => "Rossi",
-            "anni" => 23,
-            "voto_medio" => 8.5,
-            "linguaggio_preferito" => "PHP",
-            "immagine" => "https://source.unsplash.com/random/200x200?person", -->
+// ! Filtro per voto massimo.
+if (isset($_GET['maxVote'])) {
+    foreach ($classi as $classElementsValue) {
+        foreach ($classElementsValue as $classMember) {
+            if ($classMember['voto_medio'] <= $_GET['maxVote']) {
+                array_push($bestStudents, $classMember);
+            }
+        }
+    }
+
+    array_push($bestStudentsInClass, $bestStudents);
+    array_push($bestStudentsInClasses, $bestStudents);
+
+    $classi = $bestStudentsInClasses;
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -46,27 +52,44 @@ $bestStudentsInClasses = [];
 
 <body>
     <main>
+        <div class="title">
+            <h1>
+                Classi anno scolastico 24/25
+            </h1>
+        </div>
+
+        <!-- Form -->
+        <h3>Filtro</h3>
+        <form action="" method="GET">
+            <label for="maxVote">Cerca per voto massimo</label>
+            <input type="number" min=0 max=10 name="maxVote">
+            <button>Filtra</button>
+        </form>
+
+        <!-- Cicli elenco classi e studenti -->
         <div class="classe">
             <?php foreach ($classi as $className => $classElementsValue) { ?>
-            <h2><?= $className; ?></h2>
-            <ul>
-                <?php foreach ($classElementsValue as $classMember) {  ?>
-                <li class="student-number">
-                    <h4>Studente: <?= $classMember['id'] ?></h4>
-                    <ul>
-                        <?php foreach ($classMember as $classElementKey => $classElementValue) { ?>
-                        <li>
-                            <?php $classElementKeyUpperCase = ucfirst($classElementKey);
+                <h2><?= $className !== 0 ? $className : ''; ?></h2>
+                <ul>
+                    <?php foreach ($classElementsValue as $classMember) {  ?>
+                        <li class="student-number">
+                            <h4>Studente: <?= $classMember['id'] ?></h4>
+                            <ul>
+                                <?php foreach ($classMember as $classElementKey => $classElementValue) { ?>
+                                    <li>
+                                        <?php $classElementKeyUpperCase = ucfirst($classElementKey);
                                         $classElementKeyComplete = str_replace('_', ' ', $classElementKeyUpperCase);
                                         echo "{$classElementKeyComplete}: {$classElementValue}";
                                         ?>
+                                    </li>
+
+                                <?php } ?>
+                                <hr>
+                            </ul>
                         </li>
-                        <?php } ?>
-                    </ul>
-                </li>
-                <?php } ?>
-            </ul>
-            <hr>
+                    <?php } ?>
+                </ul>
+                <hr>
             <?php } ?>
         </div>
     </main>
