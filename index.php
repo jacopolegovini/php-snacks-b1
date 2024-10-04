0e023f3611ae44447743fa995508c9e3eb45fcae
jacopolegovini <legovini96@gmail.com>
2024-10-04 11:46:32 +0200

    html structured

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <main>
+        <form action="" method="GET">
+            <input type="text" name="name" placeholder="Inserisci il nome">
+            <input type="email" name="email" placeholder="Inserisci la tua mail">
+            <input type="number" name="age" placeholder="Inserisci la tua età">
+        </form>
+    </main>
+</body>
+
+</html>
